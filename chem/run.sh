c43b10acabb8fc23e38ccf21d92011a548ded578
LanceKnight <lanceknight26@gmail.com>
2021-06-20 09:29:09 -0500

    changed run command

--- chem/run.sh
+++ chem/run.sh
@@ -1,1 +1,1 @@
-python finetune.py --dataset esol --seed 10 --runseed 10 --input_model_file model_gin/masking_element_embedding.pth --dropout_ratio=0
+python finetune.py --dataset hiv --seed 10 --runseed 10 --input_model_file model_gin/masking_element_embedding.pth --dropout_ratio=0
